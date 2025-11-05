dee9272783ab5bbe69ac86239cfad7c5121f997d
Gabriel Andres Brandalisse <gabibrandalisse@gmail.com>
2025-11-04 22:35:37 -0300

    add validation pipe to validate incoming request dto

--- src/pet-size/dto/create-pet-size.dto.ts
+++ src/pet-size/dto/create-pet-size.dto.ts
@@ -1,3 +1,6 @@
+import { IsString } from 'class-validator';
+
 export class CreatePetSizeDto {
+  @IsString()
   name: string;
 }
--- src/pet-type/dto/create-pet-type.dto.ts
+++ src/pet-type/dto/create-pet-type.dto.ts
@@ -1,3 +1,6 @@
+import { IsString } from 'class-validator';
+
 export class CreatePetTypeDto {
+  @IsString()
   name: string;
 }

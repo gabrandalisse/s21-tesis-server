12850ab19cd92516cbe312b313dd30fd4556d135
Gabriel Andres Brandalisse <gabibrandalisse@gmail.com>
2025-11-12 17:53:47 -0300

    report resource created

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { PetModule } from './pet/pet.module';
 import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './user/user.module';
+import { ReportModule } from './report/report.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), PetModule, UserModule],
+  imports: [ConfigModule.forRoot(), PetModule, UserModule, ReportModule],
   controllers: [],
   providers: [],
 })

fb88371ce2bf734b2cdcbab8b685a01323d2eea5
Gabriel Andres Brandalisse <gabibrandalisse@gmail.com>
2025-11-13 19:11:26 -0300

    start creating report prisma model

--- src/report/report.module.ts
+++ src/report/report.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ReportController } from './controllers/report.controller';
 import { ReportService } from './services/report.service';
+import { DatabaseModule } from 'src/database/database.module';
 
 @Module({
   controllers: [ReportController],
   providers: [ReportService],
+  imports: [DatabaseModule],
 })
 export class ReportModule {}
